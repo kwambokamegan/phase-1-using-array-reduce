ad9c88b3dc640fa441ae54d11d3cab0786618930
Megan <megan.nyakina@student.moringaschool.com>
2024-07-11 00:33:44 +0300

    adds using array reduce

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Function to calculate the total number of batteries using reduce
+function getTotalBatteries(batteryBatches) {
+    const totalBatteries = batteryBatches.reduce(function(total, batch) {
+      return total + batch;
+    }, 0); // Initial value of total is 0
+    
+    return totalBatteries;
+  }
+  
+  // Assigning the result of getTotalBatteries to totalBatteries variable
+  const totalBatteries = getTotalBatteries(batteryBatches);
+  
+  // Testing the function
+  console.log(totalBatteries); // Output: 31
